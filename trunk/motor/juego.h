161654c74487f8db9be003e72f1b16b303ba9c6f
david.saltares <david.saltares@3aefce1c-a7af-dadf-8411-c36b2e3d61d9>
2010-07-05 19:36:54 +0000

    Documentación de Juego

--- trunk/motor/juego.h
+++ trunk/motor/juego.h
@@ -5,22 +5,73 @@
 #include "marcador.h"
 #include "bola.h"
 
+/**
+	@struct tipoJuego
+	
+	@brief Control del juego, se utiliza Juego
+*/
 typedef struct{
+	/** Pong del jugador 1 */
 	Pong pong1;
+	/** Pong del jugador 2 */
 	Pong pong2;
+	/** Bola de juego */
 	Bola bola;
+	/** Marcador con las puntuaciones */
 	Marcador marcador;
+	/** Imagen con la mesa de juego */
 	SDL_Surface* mesa;
+	/** Superficie de la pantalla */
 	SDL_Surface* pantalla;
+	/** Estado del teclado */
 	Uint8* teclado;
 }tipoJuego;
 
+/**
+	@struct Juego
+	
+	@brief Control del juego
+	
+	Estructura que contiene los elementos del juego, los inicializa, destruye y controla el bucle principal.
+	
+	La documentación de las funciones de este TAD en juego.h
+	
+	Ejemplo de uso:
+	
+	\code
+		int main(int argc, char **argv)
+		{
+			Juego juego;
+			
+			juego = Juego_crear();
+			Juego_bucle_principal(juego);
+			Juego_destruir(juego);
+			
+			return 0;
+		}
+	\endcode
+*/
 typedef tipoJuego* Juego;
 
+/**
+	@return juego inicializado
+	
+	Inicializa SDL, crea y devuelve la estructura del juego
+*/
 Juego Juego_crear(void);
 
+/**
+	@param juego Juego a ser destruido
+	
+	Libera la memoria ocupada por el juego y cierra la SDL
+*/
 void Juego_destruir(Juego juego);
 
+/**
+	@param juego juego inicializado sobre el que ejecutar el bucle principal
+	
+	Ejecuta el bucle principal de juego
+*/
 void Juego_bucle_principal(Juego juego);
 
 #endif
