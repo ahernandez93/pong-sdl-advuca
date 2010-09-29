681f1d89064e5949f505ad0c1d414b7f3f9aac0a
david.saltares <david.saltares@3aefce1c-a7af-dadf-8411-c36b2e3d61d9>
2010-09-29 17:07:04 +0000

    Añade paso 1 del taller (pantalla en blanco y entrada)

---
+++ trunk/branches/paso1/main.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+#include "motor/juego.h"
+
+int main(int argc, char **argv)
+{
+	Juego juego;
+	
+	juego = Juego_crear();
+	Juego_bucle_principal(juego);
+	Juego_destruir(juego);
+	
+	return 0;
+}
