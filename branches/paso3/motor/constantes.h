a5ee9d84a5f2ecb9595bec3d2fbdce30ef790211
david.saltares <david.saltares@3aefce1c-a7af-dadf-8411-c36b2e3d61d9>
2010-09-30 14:27:18 +0000

    Añade el paso3 del taller (pongs con entrada de usuario)

---
+++ branches/paso3/motor/constantes.h
@@ -0,0 +1,22 @@
+#ifndef _CONSTANTES_
+#define _CONSTANTES_
+
+/** Ancho en píxeles de la pantalla */
+#define PANTALLA_ANCHO	640
+
+/** Alto en píxeles de la pantalla */
+#define PANTALLA_ALTO	400
+
+/** Profundidad de color (bits per píxel) */
+#define BPP		32
+
+/** FPS, Frames per second (frames por segundo) */
+#define FPS		30
+
+/** Jugador 1 */
+#define J1		1
+
+/** Jugador 2 */
+#define J2		2
+
+#endif
---
+++ branches/paso3/motor/juego.h
@@ -0,0 +1,79 @@
+#ifndef _JUEGO_
+#define _JUEGO_
+
+#include <SDL/SDL.h>
+
+#include "pong.h"
+
+/**
+	@struct tipoJuego
+	
+	@brief Control del juego, se utiliza Juego
+*/
+typedef struct{
+	/** Pong del jugador 1 */
+	Pong pong1;
+	/** Pong del jugador 2 */
+	Pong pong2;
+	/** Superficie de la pantalla */
+	SDL_Surface* pantalla;
+	/** Imagen con la mesa de juego */
+	SDL_Surface* mesa;
+	/** Estado del teclado */
+	Uint8* teclado;
+	/** Control de tiempo */
+	int tiempo0;
+	/** Control de tiempo */
+	int tiempo1;
+}tipoJuego;
+
+/**
+	@struct Juego
+	
+	@brief Control del juego
+	
+	Estructura que contiene los elementos del juego, los inicializa, destruye y controla el bucle principal.
+	
+	Se define como un puntero a tipoJuego (para no tener que manejar punteros en las funciones)
+	
+	La documentación de las funciones de este TAD en juego.h
+	
+	Ejemplo de uso:
+	
+	\code
+		int main(int argc, char **argv)
+		{
+			Juego juego;
+			
+			juego = Juego_crear();
+			Juego_bucle_principal(juego);
+			Juego_destruir(juego);
+			
+			return 0;
+		}
+	\endcode
+*/
+typedef tipoJuego* Juego;
+
+/**
+	@return juego inicializado
+	
+	Inicializa SDL, crea y devuelve la estructura del juego
+*/
+Juego Juego_crear(void);
+
+/**
+	@param juego Juego a ser destruido
+	
+	Libera la memoria ocupada por el juego y cierra la SDL
+*/
+void Juego_destruir(Juego juego);
+
+/**
+	@param juego juego inicializado sobre el que ejecutar el bucle principal
+	
+	Ejecuta el bucle principal de juego
+*/
+void Juego_bucle_principal(Juego juego);
+
+#endif
