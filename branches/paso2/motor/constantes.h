d05413245e2460f238261360c0089a3ae331cd94
david.saltares <david.saltares@3aefce1c-a7af-dadf-8411-c36b2e3d61d9>
2010-09-30 14:26:40 +0000

    Añade el paso2 del taller (pantalla con fondo de juego)

---
+++ branches/paso2/motor/constantes.h
@@ -0,0 +1,13 @@
+#ifndef _CONSTANTES_
+#define _CONSTANTES_
+
+/** Ancho en píxeles de la pantalla */
+#define PANTALLA_ANCHO	640
+
+/** Alto en píxeles de la pantalla */
+#define PANTALLA_ALTO	400
+
+/** Profundidad de color (bits per píxel) */
+#define BPP		32
+
+#endif
