ca0119c026994086cfe630b3fe85c0b755e8a1d6
david.saltares <david.saltares@3aefce1c-a7af-dadf-8411-c36b2e3d61d9>
2010-10-03 09:50:04 +0000

    Añade el paso 1 sin resolver

---
+++ branches/paso1-ejercicio/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "motor/juego.h"
+
+int main(int argc, char **argv)
+{
+	Juego juego;
+	
+	/* Creamos el juego */
+	
+	/* Comenzamos el bucle principal (game loop) */
+	
+	/* Destruimos el juego (si no liberamos la memoria, seremos unos guarros) */
+	
+	return 0;
+}
