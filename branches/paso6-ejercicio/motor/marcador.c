374493bf7243052d13aca8ab1cf98c06f9e344f7
david.saltares <david.saltares@3aefce1c-a7af-dadf-8411-c36b2e3d61d9>
2010-10-03 11:08:49 +0000

    Añade el paso 6 sin resolver

---
+++ branches/paso6-ejercicio/motor/marcador.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <SDL/SDL_ttf.h>
+
+#include "marcador.h"
+#include "constantes.h"
+
+Marcador Marcador_crear(void)
+{
+	Marcador marcador;
+	
+	/* Se reserva memoria para el marcador */
+	if((marcador = (Marcador)malloc(sizeof(tipoMarcador))) == NULL){
+		printf("ERROR -> Marcador_crear(): Memoria insuficiente\n");
+		exit(1);	
+	}
+	
+	/* Posición del marcador */
+
+
+	/* Valor inicial del marcador récord y puntos actuales */
+	
+	
+	/* Le asignamos una fuente. Cargamos la fuente a tamaño 30 */
+	if((marcador->fuente = /* */) == NULL){
+		printf("ERROR -> Marcador_crear(): No se pudo inicializar la fuente multimedia/marcador.ttf\n");
+		exit(1);
+	}
+	
+	/* Le indicamos el color de la fuente (blanco) */
+	marcador->color.r = 255;
+	marcador->color.g = 255;
+	marcador->color.b = 255;
+	
+	return marcador;
+}
+
+void Marcador_destruir(Marcador marcador)
+{
+	/* Destuimos la fuente cargada */
+	
+	
+	/* Liberamos la memoria de marcador */
+	free(marcador);
+}
+
+void Marcador_incrementar(Marcador marcador)
+{
+	marcador->p_actuales++;
+	
+	if(marcador->p_actuales > marcador->p_record)
+		marcador->p_record = marcador->p_actuales;
+}
+
+void Marcador_reiniciar(Marcador marcador)
+{
+	marcador->p_actuales = 0;
+}
+
+void Marcador_dibujar(Marcador marcador, SDL_Surface* pantalla)
+{
+	char buffer[30];
+	SDL_Surface* temporal;
+	SDL_Surface* superficie;
+	SDL_Rect destino;
+	
+	/* Puntos actuales */
+	sprintf(buffer, "Actuales: %d", marcador->p_actuales);
+	
+	/* Renderizamos el texto en una superficie temporal */
+	if((temporal = TTF_RenderUTF8_Solid(marcador->fuente, buffer, marcador->color)) == NULL){
+		printf("ERROR -> Marcador_dibujar(): No se pudo renderizar el texto\n");
+		exit(1);
+	}
+
+	/* Adaptamos la superficie a la pantalla */
+	if((superficie = SDL_DisplayFormat(temporal)) == NULL){
+		printf("ERROR -> Marcador_dibujar(): No se pudo adaptar el texto renderizado a la pantalla\n");
+		exit(1);
+	}
+	
+	destino.x = marcador->x;
+	destino.y = marcador->y;
+	destino.w = superficie->w;
+	destino.h = superficie->h;
+	
+	/* Volcamos la superficie en la pantalla */
+	SDL_BlitSurface(superficie, NULL, pantalla, &destino);
+	
+	/* Liberamos las superficies temporales */
+	SDL_FreeSurface(temporal);
+	SDL_FreeSurface(superficie);
+	
+	/* Puntos récord */
+	sprintf(/* */);
+	
+	/* Renderizamos el texto en una superficie temporal */
+	if((temporal = /* */) == NULL){
+		printf("ERROR -> Marcador_dibujar(): No se pudo renderizar el texto\n");
+		exit(1);
+	}
+
+	/* Adaptamos la superficie temporal a la pantalla */
+	if((superficie = /* */) == NULL){
+		printf("ERROR -> Marcador_dibujar(): No se pudo adaptar el texto renderizado a la pantalla\n");
+		exit(1);
+	}
+	
+	destino.x = marcador->x + 325;
+	destino.y = marcador->y;
+	destino.w = superficie->w;
+	destino.h = superficie->h;
+	
+	/* Volcamos la superficie en la pantalla */
+	
+	
+	
+	/* Liberamos superficies temporales */
+	SDL_FreeSurface(temporal);
+	SDL_FreeSurface(superficie);
+}
