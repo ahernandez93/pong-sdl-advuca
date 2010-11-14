6376c787fc896e2d976d0910e1ac7a0fba66edd4
david.saltares <david.saltares@3aefce1c-a7af-dadf-8411-c36b2e3d61d9>
2010-11-14 15:01:33 +0000

    Añade proyectos CodeBlocks

--- trunk/branches/paso4/motor/juego.c
+++ trunk/branches/paso4/motor/juego.c
@@ -5,7 +5,6 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_ttf.h>
-#include <SDL/SDL_mixer.h>
 
 #include "juego.h"
 #include "constantes.h"
@@ -16,16 +15,16 @@
 SDL_Surface *Juego_iniciar_SDL(void){
 
 	SDL_Surface* auxiliar;
-	
+
 	/* Inicializamos SDL */
 	if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
 		printf("ERROR -> Juego_iniciar_SDL(): no se pudo iniciar SDL\n");
 		exit(1);
 	}
-	
+
 	/* Al salir, cerramos SDL */
 	atexit(SDL_Quit);
-	
+
 	/* Cteamos la superficie principal */
 	auxiliar = SDL_SetVideoMode(PANTALLA_ANCHO, PANTALLA_ALTO, BPP, SDL_HWSURFACE | SDL_DOUBLEBUF);
 
@@ -33,19 +32,19 @@ SDL_Surface *Juego_iniciar_SDL(void){
 		printf("ERROR -> Juego_iniciar_SDL(): no se pudo iniciar la ventana SDL\n");
 		exit(1);
 	}
-	
+
 	/* Establecemos el título de la ventana */
 	SDL_WM_SetCaption("Pong - ADVUCA - Paso 4", NULL);
-	
+
 	/* Hacemos el cursor invisible */
 	SDL_ShowCursor(SDL_DISABLE);
-	
+
 	/* Inicializamos SDL TTF */
 	if(TTF_Init() == -1){
 		printf("ERROR -> Juego_iniciar_SDL(): no se pudo iniciar SDL TTF\n");
 		exit(1);
 	}
-	
+
 	/* Al salir, cerramos SDL TTF */
 	atexit(TTF_Quit);
 
@@ -56,10 +55,10 @@ int Juego_procesar_eventos(Juego juego)
 {
 	SDL_Event event;
 	int salir = 0;
-	
+
 	/* Actualizamos el teclado */
 	juego->teclado = SDL_GetKeyState(NULL);
-	
+
 	/* Mientras queden eventos en la cola de eventos */
 	while(SDL_PollEvent(&event)){
 		switch(event.type){
@@ -75,79 +74,79 @@ int Juego_procesar_eventos(Juego juego)
 				break;
 		}
 	}
-	
+
 	SDL_PumpEvents();
-	
-	return salir;	
+
+	return salir;
 }
 
 Juego Juego_crear(void)
 {
 	Juego juego;
-	
+
 	/* Reservamos memoria para el juego */
 	if((juego = (Juego)malloc(sizeof(tipoJuego))) == NULL){
 		printf("ERROR -> Juego_crear(): no se pudo reservar memoria para el juego\n");
 		exit(1);
 	}
-	
+
 	/* Inicializamos SDL */
 	juego->pantalla = Juego_iniciar_SDL();
-	
+
 	/* Creamos los elementos del juego */
 	juego->pong1 = Pong_crear(J1);
 	juego->pong2 = Pong_crear(J2);
 	juego->bola = Bola_crear();
 	juego->mesa = Imagen_cargar("multimedia/Fondo.png");
-	
+
 	/* Inicializamos el teclado */
 	juego->teclado = SDL_GetKeyState(NULL);
-	
+
 	/* Inicio del control de tiempo */
 	juego->tiempo0 = 0;
-	
+
 	return juego;
 }
 
 void Juego_destruir(Juego juego)
-{	
+{
 	/* Destuimos todos los elementos del juego */
 	Pong_destruir(juego->pong1);
 	Pong_destruir(juego->pong2);
 	Bola_destruir(juego->bola);
 	Imagen_borrar(juego->mesa);
-	
+
 	/* Cerramos SDL */
 	SDL_Quit();
-	
+
 	/* Liberamos la memoria de juego */
 	free(juego);
 }
 
 void Juego_control_tiempo(Juego juego)
-{	
+{
 	/* Tiempo del último frame = Tiempo total - Tiempo de frames anteriores */
 	juego->tiempo1 = SDL_GetTicks();
-	
+
 	/* Esperamos lo que nos falta para mantener 25 fps */
-	if((juego->tiempo1 - juego->tiempo0) < (1000 / FPS))	
+	if((juego->tiempo1 - juego->tiempo0) < (1000 / FPS))
 		SDL_Delay((1000 / FPS) - (juego->tiempo1 - juego->tiempo0));
-		
+
 	juego->tiempo0 = juego->tiempo1;
 }
 
 void Juego_comprobar_colisiones(Bola bola)
 {
 	SDL_Rect rectangulo_bola;
-	
+
 	/* Obtenemos las dimensiones de la bola */
 	rectangulo_bola = Bola_rectangulo_colision(bola);
-	
+
 	/* Comprobamos si hay colisión con el extremo superior e inferior */
 	if(rectangulo_bola.y <= 0 || (rectangulo_bola.y+rectangulo_bola.h) >= PANTALLA_ALTO){
 		Bola_invertir_velocidadY(bola);
 	}
-	
+
 	/* Comprobamos si ha entrado en alguna de las dos porterías */
 	else if(rectangulo_bola.x <= 0){
 		Bola_invertir_velocidadX(bola);
@@ -162,36 +161,36 @@ void Juego_comprobar_colisiones(Bola bola)
 void Juego_bucle_principal(Juego juego)
 {
 	int salir = 0;
-	
+
 	/* Mientras no queramos salir */
 	while(!salir){
 		/* Actualizamos la entrada y procesamos la cola de eventos */
 		salir = Juego_procesar_eventos(juego);
-		
-		/* Actualizar pong de J1 según entrada */ 
+
+		/* Actualizar pong de J1 según entrada */
 		Pong_actualizar_entrada(juego->pong1, juego->teclado);
-		
+
 		/* Actualizar Bola */
 		Bola_actualizar(juego->bola);
-		
+
 		/* Colisiones */
 		Juego_comprobar_colisiones(juego->bola);
-		
+
 		/* Dibujar Mesa */
 		Imagen_dibujar(juego->mesa, juego->pantalla, 0, 0);
-		
+
 		/* Dibujar J1 */
 		Pong_dibujar(juego->pong1, juego->pantalla);
-	
+
 		/* Dibujar J2 */
 		Pong_dibujar(juego->pong2, juego->pantalla);
-		
+
 		/* Dibujar Bola */
 		Bola_dibujar(juego->bola, juego->pantalla);
-		
+
 		/* Flipping, intercambio de superficies (mostramos la pantalla) */
 		SDL_Flip(juego->pantalla);
-		
+
 		/* Control de tiempo */
 		Juego_control_tiempo(juego);
 	}
--- trunk/branches/paso5/motor/juego.c
+++ trunk/branches/paso5/motor/juego.c
@@ -5,7 +5,6 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_ttf.h>
-#include <SDL/SDL_mixer.h>
 
 #include "juego.h"
 #include "constantes.h"
@@ -16,16 +15,16 @@
 SDL_Surface *Juego_iniciar_SDL(void){
 
 	SDL_Surface* auxiliar;
-	
+
 	/* Inicializamos SDL */
 	if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
 		printf("ERROR -> Juego_iniciar_SDL(): no se pudo iniciar SDL\n");
 		exit(1);
 	}
-	
+
 	/* Al salir, cerramos SDL */
 	atexit(SDL_Quit);
-	
+
 	/* Cteamos la superficie principal */
 	auxiliar = SDL_SetVideoMode(PANTALLA_ANCHO, PANTALLA_ALTO, BPP, SDL_HWSURFACE | SDL_DOUBLEBUF);
 
@@ -33,19 +32,19 @@ SDL_Surface *Juego_iniciar_SDL(void){
 		printf("ERROR -> Juego_iniciar_SDL(): no se pudo iniciar la ventana SDL\n");
 		exit(1);
 	}
-	
+
 	/* Establecemos el título de la ventana */
 	SDL_WM_SetCaption("Pong - ADVUCA - Paso 5", NULL);
-	
+
 	/* Hacemos el cursor invisible */
 	SDL_ShowCursor(SDL_DISABLE);
-	
+
 	/* Inicializamos SDL TTF */
 	if(TTF_Init() == -1){
 		printf("ERROR -> Juego_iniciar_SDL(): no se pudo iniciar SDL TTF\n");
 		exit(1);
 	}
-	
+
 	/* Al salir, cerramos SDL TTF */
 	atexit(TTF_Quit);
 
@@ -56,10 +55,10 @@ int Juego_procesar_eventos(Juego juego)
 {
 	SDL_Event event;
 	int salir = 0;
-	
+
 	/* Actualizamos el teclado */
 	juego->teclado = SDL_GetKeyState(NULL);
-	
+
 	/* Mientras queden eventos en la cola de eventos */
 	while(SDL_PollEvent(&event)){
 		switch(event.type){
@@ -75,42 +74,42 @@ int Juego_procesar_eventos(Juego juego)
 				break;
 		}
 	}
-	
+
 	SDL_PumpEvents();
-	
-	return salir;	
+
+	return salir;
 }
 
 Juego Juego_crear(void)
 {
 	Juego juego;
-	
+
 	/* Reservamos memoria para el juego */
 	if((juego = (Juego)malloc(sizeof(tipoJuego))) == NULL){
 		printf("ERROR -> Juego_crear(): no se pudo reservar memoria para el juego\n");
 		exit(1);
 	}
-	
+
 	/* Inicializamos SDL */
 	juego->pantalla = Juego_iniciar_SDL();
-	
+
 	/* Creamos los elementos del juego */
 	juego->pong1 = Pong_crear(J1);
 	juego->pong2 = Pong_crear(J2);
 	juego->bola = Bola_crear();
 	juego->mesa = Imagen_cargar("multimedia/Fondo.png");
-	
+
 	/* Inicializamos el teclado */
 	juego->teclado = SDL_GetKeyState(NULL);
-	
+
 	/* Inicio del control de tiempo */
 	juego->tiempo0 = 0;
-	
+
 	return juego;
 }
 
 void Juego_destruir(Juego juego)
-{	
+{
 	/* Destuimos todos los elementos del juego */
 	Pong_destruir(juego->pong1);
 	Pong_destruir(juego->pong2);
@@ -119,20 +118,20 @@ void Juego_destruir(Juego juego)
 
 	/* Cerramos SDL */
 	SDL_Quit();
-	
+
 	/* Liberamos la memoria de juego */
 	free(juego);
 }
 
 void Juego_control_tiempo(Juego juego)
-{	
+{
 	/* Tiempo del último frame = Tiempo total - Tiempo de frames anteriores */
 	juego->tiempo1 = SDL_GetTicks();
-	
+
 	/* Esperamos lo que nos falta para mantener 25 fps */
-	if((juego->tiempo1 - juego->tiempo0) < (1000 / FPS))	
+	if((juego->tiempo1 - juego->tiempo0) < (1000 / FPS))
 		SDL_Delay((1000 / FPS) - (juego->tiempo1 - juego->tiempo0));
-		
+
 	juego->tiempo0 = juego->tiempo1;
 }
 
@@ -145,7 +144,7 @@ int Juego_colisiones_rectangulo(SDL_Rect rect1, SDL_Rect rect2)
 		return 1;
 	else
 		return 0;
-		
+
 }
 
 void Juego_comprobar_colisiones(Bola bola, Pong pong1, Pong pong2)
@@ -153,7 +152,7 @@ void Juego_comprobar_colisiones(Bola bola, Pong pong1, Pong pong2)
 	SDL_Rect rectangulo_bola;
 	SDL_Rect rectangulo_pong1;
 	SDL_Rect rectangulo_pong2;
-	
+
 	/* Obtenemos las dimensiones de la bola y el pong */
 	rectangulo_bola = Bola_rectangulo_colision(bola);
 	rectangulo_pong1 = Pong_rectangulo_colision(pong1);
@@ -168,7 +167,7 @@ void Juego_comprobar_colisiones(Bola bola, Pong pong1, Pong pong2)
 	else if(Juego_colisiones_rectangulo(rectangulo_bola, rectangulo_pong1)){
 			Bola_invertir_velocidadX(bola);
 	}
-		
+
 	/* Comprobamos si hay colisión con el pong2 */
 	else if(Juego_colisiones_rectangulo(rectangulo_bola, rectangulo_pong2)){
 			Bola_invertir_velocidadX(bola);
@@ -189,39 +188,39 @@ void Juego_comprobar_colisiones(Bola bola, Pong pong1, Pong pong2)
 void Juego_bucle_principal(Juego juego)
 {
 	int salir = 0;
-	
+
 	/* Mientras no queramos salir */
 	while(!salir){
 		/* Actualizamos la entrada y procesamos la cola de eventos */
 		salir = Juego_procesar_eventos(juego);
-		
-		/* Actualizar pong de J1 según entrada */ 
+
+		/* Actualizar pong de J1 según entrada */
 		Pong_actualizar_entrada(juego->pong1, juego->teclado);
-		
+
 		/* Actualizar pong de J2 según IA */
 		Pong_actualizar_ia(juego->pong2, juego->bola);
-		
+
 		/* Actualizar Bola */
 		Bola_actualizar(juego->bola);
-		
+
 		/* Colisiones */
 		Juego_comprobar_colisiones(juego->bola, juego->pong1, juego->pong2);
-		
+
 		/* Dibujar Mesa */
 		Imagen_dibujar(juego->mesa, juego->pantalla, 0, 0);
-		
+
 		/* Dibujar J1 */
 		Pong_dibujar(juego->pong1, juego->pantalla);
-	
+
 		/* Dibujar J2 */
 		Pong_dibujar(juego->pong2, juego->pantalla);
-		
+
 		/* Dibujar Bola */
 		Bola_dibujar(juego->bola, juego->pantalla);
-		
+
 		/* Flipping, intercambio de superficies (mostramos la pantalla) */
 		SDL_Flip(juego->pantalla);
-		
+
 		/* Control de tiempo */
 		Juego_control_tiempo(juego);
 	}
